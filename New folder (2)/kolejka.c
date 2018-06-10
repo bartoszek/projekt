19cf67698122bf2dd3a4d8749664356c92d0b7b5
bartus <szczepaniak.bartek+github@gmail.com>
2018-06-10 17:32:48 +0200

    remove_by_index implement

--- New folder (2)/kolejka.c
+++ New folder (2)/kolejka.c
@@ -24,32 +24,38 @@ int push(Queue *queue , void* data)
         queue->tail->next=newNode; // [head (next=second_node)] [second_node]... [tail (next=NULL)] >>> ... [tail(next=newNode)]                             (set newNode as next after tail)
         queue->tail=newNode; //       [head (next=second_node)] [second_node]... [tail(next=newNode)] >>> ... [second_to_last (next=newNode)] [tail=newNode] (set tail node to newNode)
     }
+    return 0;
 }
 
-int remove_by_index(node_t ** head, int n) 
+int remove_by_index(Queue *queue, int n) //index for 1 to queue->sizeOfQueue
 {
-    int i = 0;
-    int retval = -1;
-    node_t * current = *head;
-    node_t * temp_node = NULL;
-
-    if (n == 0) {
-        return pop(head);
-    }
-
-    for (i = 0; i < n-1; i++) 
+    if(n<=queue->sizeOfQueue)
     {
-        if (current->next == NULL) 
+        if(n==1) //remove head
         {
-            return -1;
+            node_t *to_remove=queue->head;
+            queue->head=to_remove->next;
+            free(to_remove);
         }
-        current = current->next;
+        else //remove further away
+        {
+            node_t *to_remove=queue->head->next; //remove next after head
+            node_t *to_reconect=queue->head;     //recconect with head
+            for (int i = 1; i < n; i++)          //iterate n-1 times moving remove and recconect to next elem
+            {
+                to_remove=to_remove->next;
+                to_reconect=to_reconect->next;
+            }
+            to_reconect=to_reconect->next;       //reconnect previus to remove with next after remove ... [to_reconnect] [to_remove] [to_remove->next]
+            free(to_remove);
+        }
+    }
+    else
+    {
+        fprintf ( stderr , "index out of range in queue.remove_by_index \n " );
+        return ( 1 );
     }
 
-    temp_node = current->next;
-    retval = temp_node->val;
-    current->next = temp_node->next;
-    free(temp_node);
 
     return retval;
 
